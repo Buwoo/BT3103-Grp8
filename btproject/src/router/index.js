c49eabf1592c1b6a5c98a76060f4c58f3c4edd0b
Bryanwoo <bubu.woo7999@gmail.com>
2022-03-12 00:27:13 +0800

    Auth issues fix + Vuex
    Fixed auth issues with firestore auth
    Added vuex for future use

--- btproject/src/router/index.js
+++ btproject/src/router/index.js
@@ -4,6 +4,7 @@ import HawkerExploreView from "@/views/HawkerExploreView.vue";
 import NEADashboardView from "@/views/NEADashboardView.vue";
 import HawkerProfileView from "@/views/HawkerProfileView.vue";
 import TenderFormView from "@/views/TenderFormView.vue";
+import { getAuth } from "firebase/auth";
 
 const routes = [
   {
@@ -12,30 +13,55 @@ const routes = [
     component: LoginPageView,
   },
   {
-    path: "/hawker/explore/:id",
+    path: "/hawker/explore",
     name: "HawkerExploreView",
     component: HawkerExploreView,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
-    path: "/NEA/dashboard/:id",
+    path: "/NEA/dashboard",
     name: "NEADashboardView",
     component: NEADashboardView,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
-    path: '/hawker/profile/:id',
-    name: 'HawkerProfile',
+    path: "/hawker/profile",
+    name: "HawkerProfile",
     component: HawkerProfileView,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
-    path: '/hawker/dashboard/tenderForm/:id/:tenderID',
-    name: 'TenderFormView',
+    // path: '/hawker/dashboard/tenderForm/:id/:tenderID', //@Jon I changed this part to remove the :id
+    path: "/hawker/dashboard/tenderForm/:tenderID",
+    name: "TenderFormView",
     component: TenderFormView,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (getAuth().currentUser) {
+      next();
+    } else {
+      next("/");
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ btproject/src/uifire.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAu15K6Kx_D_gpRvpoxBMAfP-tnRujzMaY",
+  authDomain: "hawkerapp-15d57.firebaseapp.com",
+  projectId: "hawkerapp-15d57",
+  storageBucket: "hawkerapp-15d57.appspot.com",
+  messagingSenderId: "515992126924",
+  appId: "1:515992126924:web:07ccd43a389ef815790969",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
