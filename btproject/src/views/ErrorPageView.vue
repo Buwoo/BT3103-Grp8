81d93a583fc10934355cd5f7d2ca9dd6f54f069f
Bryanwoo <bubu.woo7999@gmail.com>
2022-03-14 17:55:25 +0800

    404 Page Route + Dummy Page

---
+++ btproject/src/views/ErrorPageView.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Page Not Found</h1>
+  <br />
+  <button @click="backFunction">Go Back</button>
+  <br />
+</template>
+
+<script>
+import router from "../router/index.js";
+
+export default {
+  name: "ErrorPageView",
+  methods: {
+    backFunction() {
+      router.back();
+    },
+  },
+};
+</script>
+
+<style scoped></style>
