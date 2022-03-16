f035e0c3e5e1b896ace65b8e948a7f14dcaa66b6
sarahjingwen <spongebob1894@gmail.com>
2022-03-16 23:16:25 +0800

    Fix Profile CSS and fetching data

--- btproject/src/views/HawkerProfileView.vue
+++ btproject/src/views/HawkerProfileView.vue
@@ -1,69 +1,92 @@
 <template>
-  <HawkerNavBar :name = "name"/>
-  <div class="wrapper"> 
-  <h1><b>Profile</b></h1>
-  <ProfileCard 
-  :name="name" 
-  :address="address"
-  :email="email"
-  :mobile="mobile"
-  :ic="ic"/>
-  </div> 
+  <HawkerNavBar :name="name" />
+  <div class="wrapper">
+    <h1><b>Profile</b></h1>
+    <ProfileCard
+      :name="name"
+      :address="address"
+      :email="email"
+      :mobile="mobile"
+      :ic="ic"
+    />
+  </div>
 </template>
 
 <script>
-import HawkerNavBar from "../components/HawkerNavBar.vue"
-import ProfileCard from "../components/ProfileCard.vue"
-import firebaseApp from '../firebase.js'
-import { getFirestore, getDoc, doc } from "firebase/firestore"
+import HawkerNavBar from "../components/HawkerNavBar.vue";
+import ProfileCard from "../components/ProfileCard.vue";
+import firebaseApp from "../firebase.js";
+import { getFirestore, getDoc, doc } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import firebase from "@/uifire.js";
+import router from "../router/index.js";
 
 const db = getFirestore(firebaseApp);
 
 export default {
-    components: {
-        HawkerNavBar,
-        ProfileCard,
-    },
+  components: {
+    HawkerNavBar,
+    ProfileCard,
+  },
 
-    methods: {
-      fetchData(){
-        let response = getDoc(doc(db, "Authentication", this.$route.params.id));
-        response.then((rsp) => {
+  methods: {
+    fetchData(ic) {
+      let response = getDoc(doc(db, "Authentication", ic));
+      response
+        .then((rsp) => {
           let profile = rsp.data();
           this.name = profile.fullName;
           this.email = profile.email;
           this.address = profile.address;
           this.mobile = profile.mobileNr;
-          this.ic = this.$route.params.id;
         })
         .catch((error) => {
           console.log(error);
-        })
-      }
+        });
     },
+  },
 
-    mounted(){
-      this.fetchData()
-    },
-
-    data(){
-      return {
-        name: this.name,
-        email: this.email,
-        address: this.address,
-        mobile: this.mobile,
-        ic: this.ic,
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        firebase
+          .auth()
+          .currentUser.getIdTokenResult()
+          .then((tokenResult) => {
+            console.log(tokenResult.claims);
+            if (tokenResult.claims.NEAAgent) {
+              router.push("/404");
+            }
+          });
+        this.user = user;
+        console.log(this.user);
+        this.ic = this.user.email.slice(0, 4).toUpperCase();
+        console.log(this.ic);
+        this.fetchData(this.ic);
       }
-    }
-}
+    });
+  },
+
+  data() {
+    return {
+      name: this.name,
+      email: this.email,
+      address: this.address,
+      mobile: this.mobile,
+      ic: this.ic,
+    };
+  },
+};
 </script>
 
-<style>
-.wrapper{
+<style scoped>
+.wrapper {
   text-align: left;
 }
 
-h1{
-  padding: 65px 65px 20px 65px;
+h1 {
+  position: static;
+  padding: 30px 40px 10px 40px;
 }
 </style>
