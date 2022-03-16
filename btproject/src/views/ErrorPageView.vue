34a43ffa48fd63336c954e7e80000dad5d9172c9
sarahjingwen <spongebob1894@gmail.com>
2022-03-16 16:43:14 +0800

    Add 404 error page

--- btproject/src/views/ErrorPageView.vue
+++ btproject/src/views/ErrorPageView.vue
@@ -1,12 +1,23 @@
 <template>
-  <h1>Page Not Found</h1>
+  <HawkerNavBar/>
+  <div class="container">
+        <div class="row justify-content-center">
+            <div class="text-center">
+                <span class="display-1 d-block">404</span>
+                <div class="mb-4 text-muted">The page you are looking for was not found.</div>
+                <button type="button" class="btn btn-primary" @click="backFunction">Go Back</button>
+            </div>
+        </div>
+    </div>
+  <h1></h1>
   <br />
-  <button @click="backFunction">Go Back</button>
+
   <br />
 </template>
 
 <script>
 import router from "../router/index.js";
+import HawkerNavBar from "../components/HawkerNavBar.vue"
 
 export default {
   name: "ErrorPageView",
@@ -15,7 +26,14 @@ export default {
       router.back();
     },
   },
+  components: {
+    HawkerNavBar,
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container{
+  margin-top: 30px;
+}
+</style>
