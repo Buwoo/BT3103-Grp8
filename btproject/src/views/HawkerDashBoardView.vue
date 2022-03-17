705b66180cbcfc58dcb457d1f6c3e8a02f6e1129
ashleyleerx <ashleyleeruoxin@gmail.com>
2022-03-17 21:09:05 +0800

    Styling
    Fixed for most parts except the table

--- btproject/src/views/HawkerDashBoardView.vue
+++ btproject/src/views/HawkerDashBoardView.vue
@@ -1,11 +1,6 @@
 <template>
 <HawkerNavBar/>
-<HawkerDashBoard 
-  :name="name" 
-  :address="address"
-  :email="email"
-  :mobile="mobile"
-  :id="id"/>
+<HawkerDashBoard/>
 
 </template>
 
@@ -14,10 +9,7 @@
 
 import HawkerNavBar from '@/components/HawkerNavBar.vue'
 import HawkerDashBoard from '@/components/HawkerDashBoard.vue'
-import firebaseApp from '../firebase.js'
-import { getFirestore, getDoc, doc } from "firebase/firestore"
 
-const db = getFirestore(firebaseApp);
 
 export default {
     name: 'HawkerDashBoardView',
@@ -26,39 +18,6 @@ export default {
         HawkerNavBar,
         HawkerDashBoard
     },
-
-    data(){
-      return {
-        name: this.name,
-        address: this.address,
-        email: this.email,
-        mobile: this.mobile,
-        id: this.id,
-      }
-    },
-
-    methods: {
-      fetchData(){
-        let response = getDoc(doc(db, "Authentication", this.$route.params.id));
-        response.then((rsp) => {
-          let profile = rsp.data();
-          this.name = profile.fullName;
-          this.email = profile.email;
-          this.address = profile.address;
-          this.mobile = profile.mobileNr;
-          this.id = this.$route.params.id;
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-      }
-    },
-
-    mounted() {
-      this.fetchData()
-
-    }
-
 
 }
 
