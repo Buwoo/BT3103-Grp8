dfbfb52d55c25a4db3beaeccdfa893e53e860291
Bryanwoo <bubu.woo7999@gmail.com>
2022-03-31 21:05:11 +0800

    Update Readme and Photos
    - Include Hawker Stock Photo
    - Include Application Screenshots
    - Updated readme to include screenshots and application usage details

--- README.md
+++ README.md
@@ -12,17 +12,25 @@
 - [💻 Tech Stack](#-tech-stack)
 - [⚙️ Installation](#%EF%B8%8F-installation)
 - [🎯 Application Usage](#-application-usage)
+- [📷 Screenshots ](#-screenshots)
 - [🌚 Contributors](#-contributors)
 
 ![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/rainbow.png)
 
 ## 📝 Overview
+
 ### Current Issue: Hawker Users 👨‍🍳
+
 The process of starting a hawker business is currently very inconvenient and inefficient. There is no existing platform to aid them to find a suitable spot to set up shop. Furthermore, potential hawkers need to travel to the National Environment Agency (NEA) customer centre to submit relevant documents such as the tender form to gain approval.
+
 ### Current Issue: NEA Agents 👨‍💼
+
 NEA agents have to manually process the tender form since prospective hawkers are required to submit their tender forms physically. After which, a letter will be mailed to the prospective hawker if his/her bid has been accepted.
+
 ### Solution 💡
-We have created a centralised platform which can: 
+
+We have created a centralised platform which can:
+
 <ul>
   <li>Help potential hawkers search for a suitable stall location.</li>
   <li>Help potential hawkers submit and track stall tender application status</li>
@@ -31,42 +39,113 @@ We have created a centralised platform which can:
 
 ![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/rainbow.png)
 
-## 💻 Tech Stack 
+## 💻 Tech Stack
+
 <img src="https://github.com/devicons/devicon/blob/master/icons/firebase/firebase-plain.svg" alt="Firestore Logo" width="50" height="50"/> <img src="https://github.com/devicons/devicon/blob/master/icons/vuejs/vuejs-original.svg" alt="Vue Logo" width="50" height="50"/> <img src="https://github.com/devicons/devicon/blob/master/icons/html5/html5-original.svg" alt="HTML Logo" width="50" height="50"/> <img src="https://github.com/devicons/devicon/blob/master/icons/css3/css3-original.svg" alt="CSS Logo" width="50" height="50"/> <img src="https://github.com/devicons/devicon/blob/master/icons/javascript/javascript-original.svg" alt="Javascript Logo" width="50" height="50"/>
 
 ![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/rainbow.png)
 
 ## ⚙️ Installation
+
 ### Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
+
 ![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/rainbow.png)
 
 ## 🎯 Application Usage
+
 ### Hawker Users 👨‍🍳
+
+<ul>
+  <li>Explore Hawker Centres around Singapore based on a wide range of filters!
+    <ul>
+      <li>Region</li>
+      <li>Price</li>
+      <li>Monthly Budget</li>
+      <li>Hawker Centre Opening Hours</li>
+      <li>Food Type</li>
+      <li>Stall Availability</li>
+    </ul>
+  </li>
+  <li>Track Hawker Stall Tender Applications in Real Time!</li>
+  <li>Submit Hawker Stall Tender Applications with Ease!</li>
+</ul>
 
 ### NEA Agents 👨‍💼
 
+<ul>
+  <li>Accept or Reject Hawker Stall Tenders in Real Time and with Ease!</li>
+</ul>
+
+## 📷 Screenshots
+
+### Hawker Users 👨‍🍳
+
+##### Login Page
+
+<img src="btproject/src/assets/App_SS/login_page.png">
+<br/>
+<br/>
+
+##### Hawker Explore Page
+
+<img src="btproject/src/assets/App_SS/hawker_explore.png">
+<img src="btproject/src/assets/App_SS/hawker_explore_filters.png">
+<img src="btproject/src/assets/App_SS/hawker_explore_walkthrough.png">
+<br/>
+<br/>
+
+##### Hawker Dashboard Page
+
+<img src="btproject/src/assets/App_SS/hawker_explore.png">
+<br/>
+<br/>
+
+##### Tender Application Page
+
+<img src="btproject/src/assets/App_SS/tender_form.png">
+<br/>
+<br/>
+
+##### Tender Application Page
+
+<img src="btproject/src/assets/App_SS/tender_form.png">
+<br/>
+<br/>
+
+### NEA Agents 👨‍💼
+
+##### NEA Dashboard Page
+
+<img src="btproject/src/assets/App_SS/nea_dashboard.png">
+<br/>
+<br/>
 
 ![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/rainbow.png)
 
-## 🌚 Contributors 
+## 🌚 Contributors
+
 <a href="https://github.com/Buwoo/BT3103-Grp8/graphs/contributors">
   <img src="https://contrib.rocks/image?repo=Buwoo/BT3103-Grp8" />
 </a>
--- btproject/src/components/HawkerLocationCard.vue
+++ btproject/src/components/HawkerLocationCard.vue
@@ -1,67 +1,61 @@
 <template>
-
-<div class="row g-0 py-3 border-bottom border-2">
+  <div class="row g-0 py-3 border-bottom border-2">
     <!-- Image -->
-    <div class = "col-md-4 bg-secondary">
-        <!-- Image here -->
-        <img :src="imageurl" class = "img-fluid" alt="HawkerCentreImage">
+    <div class="col-md-4 bg-secondary">
+      <!-- Image here -->
+      <img :src="imageurl" class="img-fluid" alt="HawkerCentreImage" />
     </div>
     <!-- Text Content -->
-    <div class = "col-md-8 text-secondary text-start ps-2">
-        <div>
-            <h5 id="location_card_name" class="text-dark"> {{name}} </h5>
-            <p id="location_card_region">Region: {{region}}</p>
-            <p id="location_card_address"> Address: {{address}}</p>
-            <p id="location_card_hours"> Operating Hours: {{operatingHours}}</p>
-            <p id="location_card_rental"> Monthly Rental: ${{avgMthlyCost}}</p>
-            <p id="location_card_price"> Average Price: ${{avgFoodPrice}}</p>
-            <p id="location_card_avail"> Available Stalls: {{availableNrStalls}}</p>
-            <p id="location_card_same"> Stores Sellling Food Items: {{foodType == "" || foodType == "*" ? "-" : typeOfFoodStallsNr[foodType]}}</p>
-        </div>
+    <div class="col-md-8 text-secondary text-start ps-2">
+      <div>
+        <h5 id="location_card_name" class="text-dark">{{ name }}</h5>
+        <p id="location_card_region">Region: {{ region }}</p>
+        <p id="location_card_address">Address: {{ address }}</p>
+        <p id="location_card_hours">Operating Hours: {{ operatingHours }}</p>
+        <p id="location_card_rental">Monthly Rental: ${{ avgMthlyCost }}</p>
+        <p id="location_card_price">Average Price: ${{ avgFoodPrice }}</p>
+        <p id="location_card_avail">Available Stalls: {{ availableNrStalls }}</p>
+        <p id="location_card_same">Stores Sellling Food Items: {{ foodType == "" || foodType == "*" ? "-" : typeOfFoodStallsNr[foodType] }}</p>
+      </div>
     </div>
-</div>
-     
-
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        address: String, //"2, Adam Road, Singapore 289876"
-        availableNrStalls: Number, //10
-        avgFoodPrice: Number, //5
-        avgMthlyCost: Number, // 2412
-        latitude: Number, //1.3241342225
-        longtitude: Number, //103.8141650104
-        name: String, //"Adam Road Food Centre"
-        openingHrs: String, //"0600-1800"
-        postalCode: String, //"289876"
-        region: String, //"Central"
-        streetName: String, //"Adam Road"
-        totalNrStalls: Number, //241
-        typeOfFoodStallsNr: Object, 
-        foodType: String
-        
+  props: {
+    address: String, //"2, Adam Road, Singapore 289876"
+    availableNrStalls: Number, //10
+    avgFoodPrice: Number, //5
+    avgMthlyCost: Number, // 2412
+    latitude: Number, //1.3241342225
+    longtitude: Number, //103.8141650104
+    name: String, //"Adam Road Food Centre"
+    openingHrs: String, //"0600-1800"
+    postalCode: String, //"289876"
+    region: String, //"Central"
+    streetName: String, //"Adam Road"
+    totalNrStalls: Number, //241
+    typeOfFoodStallsNr: Object,
+    foodType: String,
+  },
+  data() {
+    return {
+      imageurl: require("@/assets/hawkerimg.jpg"),
+      similarStalls: 0,
+    };
+  },
+  computed: {
+    operatingHours() {
+      let x = this.openingHrs.split("-");
+      if (this.openingHrs == "24HR") {
+        return "24HR";
+      } else {
+        return x[0] + " - " + x[1];
+      }
     },
-    data() { 
-        return {
-            //imageurl: require("@/assets/hawkerimg.jpg"),
-            imageurl: null,
-            similarStalls: 0
-        } 
-    },
-    computed: {
-        operatingHours() {
-            let x = this.openingHrs.split("-");
-            if (this.openingHrs == "24HR") {
-                return "24HR"
-            } else {
-                return x[0] + " - " + x[1];
-            }
-        }
-    }
-
-}
+  },
+};
 
 /* 
 // Mock data
@@ -75,26 +69,23 @@ availableStalls: 1,
 similarStalls: 2,
 imageurl: require("@/assets/hawkerimg.jpg")
 */
-
 </script>
 
 <style scoped>
-
 p {
-    /* Bootstrap overrides */   
-    margin-bottom: 0 !important;
-    font-size: 0.875rem !important; 
+  /* Bootstrap overrides */
+  margin-bottom: 0 !important;
+  font-size: 0.875rem !important;
 }
 
 img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: 15% 100%;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: 15% 100%;
 }
 
 .bg-select-light {
-    background-color: #ADD8E6;
+  background-color: #add8e6;
 }
-
 </style>
